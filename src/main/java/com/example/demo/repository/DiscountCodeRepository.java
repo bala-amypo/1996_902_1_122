c2c3adbc1177c079c6717676805badca0d8a2894
bala-amypo <bala@amypo.in>
2025-12-21 15:13:18 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/DiscountCodeRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.DiscountCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DiscountCodeRepository extends JpaRepository<DiscountCode, Long> {
+
+    List<DiscountCode> findByInfluencer_Id(Long influencerId);
+
+    List<DiscountCode> findByCampaign_Id(Long campaignId);
+}
---
+++ src/main/java/com/example/demo/repository/InfluencerRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Influencer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InfluencerRepository extends JpaRepository<Influencer, Long> {
+
+    Optional<Influencer> findBySocialHandle(String socialHandle);
+}
